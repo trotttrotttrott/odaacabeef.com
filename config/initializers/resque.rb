d7ec706d678d0a1067d2881730abfd09e12138c5
trotttrotttrott <trott@odaacabeef.com>
2012-11-10 20:13:32 -0200

    get rid of resque

--- config/initializers/resque.rb
+++
@@ -1,5 +0,0 @@
-# Resque.after_fork do |job|
-#   ActiveRecord::Base.connection.reconnect!
-# end
-#
-# Resque.schedule = YAML.load_file(Rails.root.join('config/resque_scheduler.yml'))
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,4 @@ Odaacabeef::Application.routes.draw do
 
   devise_for :admins
   match '/admin' => 'admin/home#index', :as => :admin_root
-  namespace :admin do
-
-    authenticate :admin do
-      mount Resque::Server.new, :at => '/resque'
-    end
-  end
-
 end
-
